ab00cdb6db1ba9d24dc2c4d51750ac544ec3c2db
krishpranav <krisna.pranav@gmail.com>
2021-03-24 16:28:27 +0530

    done changes

--- server/webui/__init__.py
+++ server/webui/__init__.py
@@ -76,3 +76,55 @@ def login():
                 else:
                     flash('Wrong passphrase')
     return render_template('login.html')
+
+@webui.route('/passchange', methods=['GET', 'POST'])
+@require_admin
+def change_password():
+    if request.method == 'POST':
+        if 'password' in request.form:
+            admin_user = User.query.filter_by(username='admin').first()
+            password_hash, salt = hash_and_salt(request.form['password'])
+            admin_user.password = password_hash
+            admin_user.salt = salt
+            db.session.add(admin_user)
+            db.session.commit()
+            flash('Password reset successfully. Please log in.')
+            return redirect(url_for('webui.login'))
+    return render_template('create_password.html')
+
+@webui.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Logged out successfully')
+
+@webui.route('/agents')
+@require_admin
+def agent_list():
+    agents = Agnet.query.order_by(Agent.last_oneline.desc())
+    return render_template('agent_list.html', agents=agents)
+
+
+@webui.route('/agents/<agent_id>')
+@require_admin
+def agent_detail(agent_id):
+    agent = Agent.query.get(agent_id)
+    if not agent:
+        abort(404)
+    return render_template('agent_detail.html', agent=agent)
+
+
+
+@webui.route('/agents/rename', methods=['POST'])
+def rename_agent():
+    if 'newname' in request.form and 'id' in request.form:
+        agent = Agent.query.get(request.form['id'])
+        if not agent:
+            abort(404)
+        agent.rename(request.form['newname'])
+    else:
+        abort(400)
+    return ''
+
+@webui.route('/uplods/<path:path>')
+def uploads(path):
+    return send_from_directory(current_app.config['UPLOAD_FOLDER'], path)
